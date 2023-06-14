fe01c2296413347641d359c4260dfb8e48f759da
UNIKIM98 <chaeeun3201@naver.com>
2023-06-14 20:00:59 +0900

    모바일 카테고리 컨트롤러 수정

--- ITitems/src/main/java/egovframework/let/cat/mob/MobCategoryController.java
+++ ITitems/src/main/java/egovframework/let/cat/mob/MobCategoryController.java
@@ -1,10 +1,14 @@
 package egovframework.let.cat.mob;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import egovframework.let.cat.service.CategoryManageVO;
@@ -18,8 +22,9 @@ public class MobCategoryController {
 	private CategoryService categoryService;
 	
 	@RequestMapping(value = "/cat/MobGetMCategoryList.do")
-	public List<CategoryVO> GetMCategoryList (CategoryManageVO categoryManageVO) throws Exception {
-		return categoryService.SelectCategoryVOList(categoryManageVO);
+	public List<CategoryVO> GetMCategoryList (@RequestBody CategoryManageVO cVO) throws Exception {
+
+		return categoryService.SelectCategoryVOList(cVO);
 	}
 
 }
